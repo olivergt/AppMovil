0f4da8189f2f4743c0ddf71695c6dab2e5a6d108
Jonathan Navarro Vega <jonathan@ranto.cl>
2022-08-23 21:43:27 -0400

    update

---
+++ BienvenidosPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppMovil
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BienvenidosPage : ContentPage
+    {
+        public BienvenidosPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Título", "Hola Mundo!", "Cancelar");
+        }
+    }
+}
